6474af09a579454901c872cfa34b0e7383d5df3b
nickyui99 <93330502+nickyui99@users.noreply.github.com>
2023-04-02 22:10:02 +0800

    update progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import {SwiperModule} from "swiper/angular";
 
 @Component({
+  schemas: [],
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, SwiperModule],
 })
 export class AppComponent {
   constructor() {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import {AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {IonicModule} from '@ionic/angular';
+
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {NgForOf, NgStyle} from "@angular/common";
+import {Navigation, SwiperOptions} from "swiper";
+import {SwiperModule} from "swiper/angular";
+
 
 @Component({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, HttpClientModule, SwiperModule, NgForOf],
 })
-export class HomePage {
-  constructor() {}
+
+
+export class HomePage implements OnInit, AfterViewInit{
+
+  data: any[]= []
+  categories : any[] = [];
+  highlights: any[] = [];
+  featured: any[] = [];
+
+  config: SwiperOptions = {
+    modules: [Navigation],
+    slidesPerView: 3.5,
+    spaceBetween: 10,
+    freeMode: true
+
+  };
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get('https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json')
+      .subscribe((res: any) => {
+        this.categories = res.categories;
+        this.highlights = res.highlights;
+        this.featured = res.featured;
+
+        console.log(res);
+      }
+    );
+  }
+
+  ngAfterViewInit(){
+  }
 }
