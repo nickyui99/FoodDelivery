2a40eab0cd4d64da5b4967cf1d7005dcb77b00d6
nicholas.ooi <nicholasooi10@gmail.com>
2023-04-03 16:09:44 +0800

    update progress

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,11 @@
-import {AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, ViewChild} from '@angular/core';
 import {IonicModule} from '@ionic/angular';
 
 import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {NgForOf, NgStyle} from "@angular/common";
-import {Navigation, SwiperOptions} from "swiper";
-import {SwiperModule} from "swiper/angular";
+import {NgForOf} from "@angular/common";
+import {Navigation, Pagination, SwiperOptions} from "swiper";
+import {SwiperComponent, SwiperModule} from "swiper/angular";
+import {RouterLink} from "@angular/router";
 
 
 @Component({
@@ -13,41 +14,63 @@ import {SwiperModule} from "swiper/angular";
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, HttpClientModule, SwiperModule, NgForOf],
+  imports: [IonicModule, HttpClientModule, SwiperModule, NgForOf, RouterLink],
 })
 
 
-export class HomePage implements OnInit, AfterViewInit{
+export class HomePage implements OnInit{
 
-  data: any[]= []
-  categories : any[] = [];
+  @ViewChild('swiper') swiper: SwiperComponent | undefined;
+
+  animationInProgress = false;
+  data: any[] = []
+  categories: any[] = [];
   highlights: any[] = [];
   featured: any[] = [];
 
-  config: SwiperOptions = {
+  categorySlideConfig: SwiperOptions = {
     modules: [Navigation],
     slidesPerView: 3.5,
     spaceBetween: 10,
-    freeMode: true
+    freeMode: true,
+    pagination: true,
 
   };
+
+  highlightSlideConfig: SwiperOptions = {
+    slidesPerView: 1.05,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+  };
+
+  featuredSlideConfig: SwiperOptions = {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    freeMode: true,
+  }
 
   constructor(private http: HttpClient) {
 
   }
 
   ngOnInit() {
+
     this.http.get('https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json')
       .subscribe((res: any) => {
-        this.categories = res.categories;
-        this.highlights = res.highlights;
-        this.featured = res.featured;
-
-        console.log(res);
-      }
-    );
+          this.categories = res.categories;
+          this.highlights = res.highlights;
+          this.featured = res.featured;
+        }
+      );
   }
 
-  ngAfterViewInit(){
+  doRefresh(event: any){
+    setTimeout(() => {
+      event.target.complete();
+    }, 2000);
+  }
+
+  ngAfterViewInit(): void {
   }
 }
